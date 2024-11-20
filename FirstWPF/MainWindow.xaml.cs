1da2b987a0b03b6f1c20105717a9d6a9f37f6ee6
Alina_Lubava <marlinoka@mail.ru>
2024-11-20 15:54:49 +0300

    Reapply "Жоско поработали"
    This reverts commit 548d6650c680c8337d940a7bdeb27f2ffde0f3d8.

--- FirstWPF/MainWindow.xaml.cs
+++ FirstWPF/MainWindow.xaml.cs
@@ -20,5 +20,10 @@ namespace FirstWPF
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }
